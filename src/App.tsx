19acf4936a17c6477e6279e26bfc875d2781955e
Ravi Dharaiya <146757250+RaviGajjar08@users.noreply.github.com>
2025-12-24 12:06:28 +0530

    Weather App

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Layout from "./components/layout";
+import { ThemeProvider } from "./context/theme-provider";
+import WeatherDashboard from "./pages/weather-dashboard";
+import CityPage from "./pages/city-page";
+import { QueryClient, QueryClientProvider} from "@tanstack/react-query";
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
+
+const queryClient = new QueryClient();
+
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+
+      <BrowserRouter>
+        <ThemeProvider defaultTheme="dark">
+         <Layout>
+          <Routes>
+            <Route path='/' element={<WeatherDashboard/>}></Route>
+            <Route path="/city/:cityName" element={<CityPage/>}></Route>
+          </Routes>
+          </Layout>
+        </ThemeProvider>
+      </BrowserRouter>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+
+
+  )
+}
+
+export default App
